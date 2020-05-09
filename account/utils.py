6cac68f355d7e0e0b890c243a54148938deb5c01
smailjak <anseunghyeon297@gmail.com>
2020-05-09 11:38:52 +0900

    account_modi

--- account/utils.py
+++ account/utils.py
@@ -1,7 +1,7 @@
 import jwt
 
-from .models                    import User
-from foodly_project.my_settings import ALGORITHM, SECRET_KEY
+from .models                    import Account
+from snack_back.my_settings import ALGORITHM, SECRET_KEY
 from django.http                import JsonResponse
 
 
