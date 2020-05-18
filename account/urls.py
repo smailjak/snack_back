2d04734096049ba9dc7cbb132d5f55cc5640ba97
blueberry333 <hiyeahin@gmail.com>
2020-05-19 01:34:48 +0900

    added commmands

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from .views      import (SignUpView,
                          SignInView,
-                         ProfileView,
-                         ActivateView)
+                         ProfileView)
 
 urlpatterns = [
     path('/sign-up', SignUpView.as_view()),
     path('/sign-in', SignInView.as_view()),
     path('/profile', ProfileView.as_view()),
-    path('/activate/<str:uidb64>/<str:token>' , ActivateView.as_view())
+    # path('/activate/<str:uidb64>/<str:token>' , ActivateView.as_view())
 ]
--- product/models.py
+++ product/models.py
@@ -16,8 +16,9 @@ class CategoryProduct(models.Model):
 class Product(models.Model):
     name             = models.CharField(max_length=250)
     image            = models.CharField(max_length=500 , null=True)
-    retail_price     = models.CharField(max_length=250)
-    price            = models.CharField(max_length=250)
+    price_strike     = models.CharField(max_length=250, default=0)
+    price_red        = models.CharField(max_length=250, default=0)
+    item_price       = models.CharField(max_length=250, default=0)
     ingredient_image = models.CharField(max_length=500 , null=True)
     delivery_guide   = models.CharField(max_length=250 , null=True)
     stock            = models.IntegerField(null=True)
